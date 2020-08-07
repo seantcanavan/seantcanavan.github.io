59c6a367c014176bc3db38ee0cbbd2ad5cfda632
seantcanavan <seantcanavan@gmail.com>
2020-08-07 06:23:59 -0700

    remove portfolio page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.scss";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Resumes from "./pages/Resumes";
-import Portfolios from "./pages/Portfolios";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
 import BlogDetails from "./pages/BlogDetails";
@@ -17,7 +16,6 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About} />
         <Route path="/resume" component={Resumes} />
-        <Route path="/portfolios" component={Portfolios} />
         <Route path="/blogs" exact component={Blogs} />
         <Route path="/blogs/blog-details/:id/:title" component={BlogDetails} />
         <Route path="/contact" component={Contact} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,6 @@ function Header(){
                     <li><NavLink exact to="/"><span>Home</span></NavLink></li>
                     <li><NavLink to="/about"><span>About</span></NavLink></li>
                     <li><NavLink to="/resume"><span>Resume</span></NavLink></li>
-                    <li><NavLink to="/portfolios"><span>Portfolios</span></NavLink></li>
                     <li><NavLink to="/blogs"><span>Blogs</span></NavLink></li>
                     <li><NavLink to="/contact"><span>Contact</span></NavLink></li>
                 </ul>
