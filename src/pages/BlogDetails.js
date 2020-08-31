2f046b61df234894c74e61b4eab1029213f07802
seantcanavan <seantcanavan@gmail.com>
2020-08-31 08:43:50 -0700

    remove disqus integration from blogs

--- src/pages/BlogDetails.js
+++ src/pages/BlogDetails.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown/with-html";
 import axios from "axios";
-import Disqus from "disqus-react";
 import Layout from "../components/Layout";
 
 function BlogDetails(props) {
   const [content, setContent] = useState("");
-  const blogId = props.match.params.id;
   const blogFile = props.match.params.title;
 
   useEffect(() => {
@@ -17,23 +15,12 @@ function BlogDetails(props) {
       })
   }, [content, blogFile]);
 
-  const disqusShortname = "chester-react"; //found in your Disqus.com dashboard
-  const disqusConfig = {
-    url: "https://tf-react-chester.now.sh/", //Homepage link of this site.
-    identifier: blogId,
-    title: blogFile
-  };
-
   return (
     <Layout>
       <div className="mi-blog-details mi-section mi-padding-top mi-padding-bottom">
         <div className="container">
           <ReactMarkdown source={content} escapeHtml={false}/>
           <div className="mi-blog-details-comments mt-30">
-            <Disqus.DiscussionEmbed
-              shortname={disqusShortname}
-              config={disqusConfig}
-            />
           </div>
         </div>
       </div>
